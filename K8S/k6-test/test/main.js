18f98889c2183c917c5434069480fba37ca2ca7d
ShadowUser17 <linuxforce17@gmail.com>
2023-06-28 01:02:45 +0300

    Remove unused deployment K8S/k6-test

--- K8S/k6-test/test/main.js
+++
@@ -1,8 +0,0 @@
-import http from 'k6/http';
-import { sleep } from 'k6';
-
-export default function () {
-    http.get('http://whoami.testing.svc:4000/bench/');
-    sleep(2);
-    http.get('http://whoami.testing.svc:4000/api/');
-}
